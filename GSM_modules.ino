54695a6068e4221da5829ebebbc27d7c745ec410
Hirshi Ahilan <78225619+Hirshikesh2003@users.noreply.github.com>
2022-04-23 19:25:26 +0530

    Add files via upload

---
+++ GSM_modules.ino
@@ -0,0 +1,26 @@
+ #include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(8, 9);
+
+void setup()
+{
+  mySerial.begin(9600);   // Setting the baud rate of GSM Module  
+  Serial.begin(9600);    // Setting the baud rate of Serial Monitor (Arduino)
+  delay(100);
+}
+
+
+void loop()
+{
+      mySerial.println("AT+CMGF=1");    //Sets the GSM Module in Text Mode
+     delay(1000);  // Delay of 1 second
+     mySerial.println("AT+CMGS=\"+919008152068\"\r"); // Replace x with mobile number
+     delay(1000);
+     mySerial.println("Thanks for the tasty biscuits");// The SMS text you want to send
+     delay(100);
+     mySerial.println((char)26);// ASCII code of CTRL+Z for saying the end of sms to  the module 
+      delay(1000);
+
+    break;
+   
+  }
